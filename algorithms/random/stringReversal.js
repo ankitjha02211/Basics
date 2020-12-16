0a1be2610b35cdb2bcb1d547a74557f4966951f9
ankit025 <ankitjha1276@gmail.com>
2020-12-16 19:16:36 +0530

    string reversal

--- algorithms/random/stringReversal.js
+++ algorithms/random/stringReversal.js
@@ -2,8 +2,26 @@
 
 // INPUT --> reverse('apple')  OUTPUT --> elppa
 
+// M1
 function reverse(str) {
   return str.split("").reverse().join("");
+}
+
+// M2
+function reverse(str) {
+  let reversed = "";
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+}
+
+// M3
+
+function reverse(str) {
+  return str.split("").reduce((reversed, char) => {
+    return char + reversed;
+  }, "");
 }
 
 let str = "apple";
