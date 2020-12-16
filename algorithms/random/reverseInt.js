a46be0c5025f1206fd23c2e5f08ebef681ae5ba7
ankit025 <ankitjha1276@gmail.com>
2020-12-16 19:31:54 +0530

    reverseInt

---
+++ algorithms/random/reverseInt.js
@@ -0,0 +1,22 @@
+// Given an integer ,return an integer that is the reverse ordering of numbers
+
+// reverseInt(51) ==> 15
+// reverseInt(-15) ==> -51
+
+function reverseInt(num) {
+  if (num === 0) {
+    return 0;
+  }
+  if (num > 0) {
+    let str = String(num);
+    return parseInt(str.split("").reverse().join(""));
+  } else {
+    let str = String(num);
+    return -parseInt(str.split("").reverse().join(""));
+  }
+}
+
+let num = 20;
+let num1 = -35;
+let result = reverseInt(num1);
+console.log(result);
