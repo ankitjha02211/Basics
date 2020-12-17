9bfb07de228a506955bf590a5d60d53a02e69609
ankit025 <ankitjha1276@gmail.com>
2020-12-17 12:38:30 +0530

    array

--- algorithms/random/arrayChunk.js
+++ algorithms/random/arrayChunk.js
@@ -1,14 +1,31 @@
 function chunk(arr, num) {
   let arr_Result = [];
   for (let i = 0; i < arr.length; ) {
-    let x = i + num ;
+    let x = i + num;
     arr_Result.push(arr.slice(i, x));
-    i = i + num ;
+    i = i + num;
   }
   return arr_Result;
 }
 
-let arr = [1, 2, 3,4];
+//M2
+// function chunk(array, size) {
+//   let chunked = [];
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+//     console.log(chunked.length - 1,last,"last")
+//     if(!last||last.length===size){
+//       console.log("if")
+//       chunked.push([element])
+//     }else{
+//       console.log("else")
+//       last.push(element)
+//     }
+//   }
+//   return chunked;
+// }
+
+let arr = [1, 2, 3, 4,5,6,7,8,9];
 let num = 3;
 let result = chunk(arr, num);
 console.log(result);
