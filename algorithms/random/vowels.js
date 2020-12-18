df301098f946e8e19687790a4b49f5d582a1dda3
ankit jha <ankitjha02211@gmail.com>
2020-12-18 12:48:34 +0530

    vow

--- algorithms/random/vowels.js
+++ algorithms/random/vowels.js
@@ -1,17 +1,25 @@
-function vowels(x) {
-  let obj = {};
-  for (let i = 0; i < x.length; i++) {
-    if (
-      x[i] == "a" ||
-      x[i] == "e" ||
-      x[i] == "i" ||
-      x[i] == "o" ||
-      x[i] == "u"
-    ) {
-      obj["vowel"] = obj['vowel'] ? obj['vowel'] + 1 : 1;
-    }
-  }
-  return obj["vowel"];
+// function vowels(x) {
+//   let obj = {};
+//   for (let i = 0; i < x.length; i++) {
+//     if (
+//       x[i] == "a" ||
+//       x[i] == "e" ||
+//       x[i] == "i" ||
+//       x[i] == "o" ||
+//       x[i] == "u"
+//     ) {
+//       obj["vowel"] = obj["vowel"] ? obj["vowel"] + 1 : 1;
+//     }
+//   }
+//   if (obj["vowel"] == undefined) {
+//     obj["vowel"] = 0;
+//   }
+//   return obj["vowel"];
+// }
+//M2
+function vowels(str) {
+  const match = str.match(/[aeiou]/gi);
+  return match ? match.length : 0;
 }
 let x = "aeiouz";
 let results = vowels(x);
