ccd8089ee6f91fdf0542056749501d48b9f647a9
ankit jha <ankitjha02211@gmail.com>
2020-12-18 09:59:27 +0530

    m2 capitalization

--- algorithms/random/capitalization.js
+++ algorithms/random/capitalization.js
@@ -1,11 +1,25 @@
-function capitalize(str) {
-  let words = [];
+// function capitalize(str) {
+//   let words = [];
 
-  for (const word of str.split(" ")) {
-    let newWord = word[0].toUpperCase() + word.slice(1);
-    words.push(newWord);
+//   for (const word of str.split(" ")) {
+//     let newWord = word[0].toUpperCase() + word.slice(1);
+//     words.push(newWord);
+//   }
+//   return words.join(" ");
+// }
+
+//M2
+function capitalize(str) {
+  let Result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i - 1] == " " || i == 0) {
+        Result += str[i].toUpperCase()
+    } else {
+        Result += str[i]
+    }
   }
-  return words.join(" ");
+  return Result
 }
 
 let str = "Test1 is for logic and Test 2 is for performance";
