7b06cad55a3acd7721acc4f5d7223cbae222ceb1
ankitjha02211 <ankitjha02211@gmail.com>
2020-12-22 21:06:17 +0530

    naive Search

--- algorithms/important/naiveSearch.js
+++ algorithms/important/naiveSearch.js
@@ -2,6 +2,7 @@ function naiveStringSearch(long, short) {
   var count = 0;
   for (let i = 0; i < long.length; i++) {
     for (let j = 0; j < short.length; j++) {
+      console.log(j, i + j);
       if (short[j] !== long[i + j]) break;
       if (j === short.length - 1) {
         count++;
