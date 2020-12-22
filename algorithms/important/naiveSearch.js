bc7b1bf3840f8dc0915f7eb5818ce58ba2d27da4
ankitjha02211 <ankitjha02211@gmail.com>
2020-12-22 20:42:59 +0530

    search Substring

---
+++ algorithms/important/naiveSearch.js
@@ -0,0 +1,15 @@
+function naiveStringSearch(long, short) {
+  var count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+let result = naiveStringSearch("lorie loled", "lol");
+console.log(result);
